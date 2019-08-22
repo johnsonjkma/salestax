ad348e61406a5354090f188bf5f833b7256681b2
johnsonjkma <54414392+johnsonjkma@users.noreply.github.com>
2019-08-22 14:04:11 -0400

    Add files via upload

---
+++ salestaxtests/SalesTaxTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SalesTax;
+
+namespace SalesTaxTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestCalculateTax_Book_Imported()
+        {
+            IProductFactory productFactory = new ProductFactory();
+            Product book = productFactory.GetProduct("book", 10, 3, GoodsType.Book, true);
+            decimal expectedTax = 1.5m;
+            var actualTax = book.CalculateItemTax() * book.Quantity;
+
+            Assert.AreEqual(expectedTax, actualTax);
+        }
+
+        [TestMethod]
+        public void TestCalculateTax_Book_NotImported()
+        {
+            IProductFactory productFactory = new ProductFactory();
+            Product book = productFactory.GetProduct("book", 10, 3, GoodsType.Book, false);
+            decimal expectedTax = 0m;
+            var actualTax = book.CalculateItemTax() * book.Quantity;
+
+            Assert.AreEqual(expectedTax, actualTax);
+        }
+
+        [TestMethod]
+        public void TestCalculateTax_Food_Imported()
+        {
+            IProductFactory productFactory = new ProductFactory();
+            Product book = productFactory.GetProduct("Pizza", 250, 1, GoodsType.Food, true);
+            decimal expectedTax = 12.5m;
+            var actualTax = book.CalculateItemTax() * book.Quantity;
+
+            Assert.AreEqual(expectedTax, actualTax);
+        }
+
+        [TestMethod]
+        public void TestCalculateTax_Food_NotImported()
+        {
+            IProductFactory productFactory = new ProductFactory();
+            Product book = productFactory.GetProduct("Shahi paneer", 250, 1, GoodsType.Food, false);
+            decimal expectedTax = 0m;
+            var actualTax = book.CalculateItemTax() * book.Quantity;
+
+            Assert.AreEqual(expectedTax, actualTax);
+        }
+
+        [TestMethod]
+        public void TestCalculateTax_Medical_Imported()
+        {
+            IProductFactory productFactory = new ProductFactory();
+            Product book = productFactory.GetProduct("Tab xyz", 2500, 1, GoodsType.Medical, true);
+            decimal expectedTax = 125m;
+            var actualTax = book.CalculateItemTax() * book.Quantity;
+
+            Assert.AreEqual(expectedTax, actualTax);
+        }
+
+        [TestMethod]
+        public void TestCalculateTax_Medical_NotImported()
+        {
+            IProductFactory productFactory = new ProductFactory();
+            Product book = productFactory.GetProduct("Tab Moov", 300, 1, GoodsType.Medical, false);
+            decimal expectedTax = 0m;
+            var actualTax = book.CalculateItemTax() * book.Quantity;
+
+            Assert.AreEqual(expectedTax, actualTax);
+        }
+
+        [TestMethod]
+        public void TestCalculateTax_Other_Imported()
+        {
+            IProductFactory productFactory = new ProductFactory();
+            Product book = productFactory.GetProduct("Car", 76547380, 1, GoodsType.Other, true);
+            decimal expectedTax = 11482107M;
+            var actualTax = book.CalculateItemTax() * book.Quantity;
+
+            Assert.AreEqual(expectedTax, actualTax);
+        }
+
+        [TestMethod]
+        public void TestCalculateTax_Other_NotImported()
+        {
+            IProductFactory productFactory = new ProductFactory();
+            Product book = productFactory.GetProduct("Tyre", 5000, 1, GoodsType.Other, false);
+            decimal expectedTax = 500m;
+            var actualTax = book.CalculateItemTax() * book.Quantity;
+
+            Assert.AreEqual(expectedTax, actualTax);
+        }
+    }
+}
