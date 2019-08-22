9757c428b8345d4b746ac7a3734ed4bfb91bedb5
johnsonjkma <54414392+johnsonjkma@users.noreply.github.com>
2019-08-22 14:02:51 -0400

    Add files via upload

---
+++ salestax/Product.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace SalesTax
+{
+
+    public enum GoodsType
+    {
+        Book = 1,
+        Food = 2,
+        Medical = 3,
+        Other = 4
+    }
+
+    public abstract class Product
+    {
+        public string Name { get; set; }
+        public bool IsImported { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public int Type { get; set; }
+
+        public Product(string name, bool isImported, decimal price, int quantity)
+        {
+            Name = name;
+            IsImported = isImported;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public decimal getPrice()
+        {
+            decimal tax = 0;
+            tax = CalculateItemTax();
+            return (Price + tax);
+        }
+
+        public decimal getItemTotal()
+        {
+            return (getPrice() * Quantity);
+        }
+
+        public virtual decimal CalculateItemTax()
+        {
+            decimal tax = 0;
+
+            if (IsImported == true)
+            {
+                tax = Convert.ToDecimal(Convert.ToDouble(Price) * 0.05);
+                tax = Math.Ceiling(tax * 20) / 20;
+            }
+
+            return tax;
+        }
+    }
+
+    public class Book : Product
+    {
+        public Book(string name, bool isImported, decimal price, int quantity)
+            : base(name, isImported, price, quantity)
+        {
+        }
+    }
+
+    public class Medical : Product
+    {
+        public Medical(string name, bool isImported, decimal price, int quantity)
+            : base(name, isImported, price, quantity)
+        {
+        }
+    }
+
+    public class Food : Product
+    {
+        public Food(string name, bool isImported, decimal price, int quantity)
+            : base(name, isImported, price, quantity)
+        {
+        }
+    }
+
+    public class Others : Product
+    {
+        public Others(string name, bool isImported, decimal price, int quantity)
+            : base(name, isImported, price, quantity)
+        {
+        }
+
+        public override decimal CalculateItemTax()
+        {
+            decimal importedTax = 0;
+            decimal tax = 0;
+
+            if (IsImported == true)
+            {
+                importedTax = Convert.ToDecimal(Convert.ToDouble(Price) * 0.05);
+            }
+
+            var salesTax = Convert.ToDecimal(Convert.ToDouble(Price) * 0.1);
+            tax = importedTax + salesTax;
+
+            tax = Math.Ceiling(tax * 20) / 20;
+
+            return tax;
+        }
+
+    }
+
+}
---
+++ salestax/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SalesTax
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            IProductFactory productFactory = new ProductFactory();
+            List<Product> cartItems = new List<Product>();
+
+            while (true)
+            {
+                Console.WriteLine("----------------- Shopping Cart -------------");
+                Console.WriteLine("Total Items: {" + cartItems.Sum(item => item.Quantity) + "}");
+                Console.WriteLine("");
+                Console.WriteLine("Add Items to Cart");
+                Console.WriteLine("");
+
+                Console.WriteLine("Name : ");
+                var name = Console.ReadLine();
+
+                Console.WriteLine("Price : ");
+                var price = Console.ReadLine();
+                try
+                {
+                    Decimal.Parse(price);
+                }
+                catch
+                {
+                    throw new Exception("Invalid price");
+                }
+
+                Console.WriteLine("Quantity : ");
+                var quantity = Console.ReadLine();
+                try
+                {
+                    Int32.Parse(quantity);
+                }
+                catch
+                {
+                    throw new Exception("Invalid quantity");
+                }
+
+                Console.WriteLine("Is imported : (Y/N) [any other key except y/Y is considered as N]");
+                var isImported = Console.ReadLine().ToLower() == "y" ? true : false;
+
+                Console.WriteLine("Type of Item (select one of below)\n1. Book\n2. Food\n3. Medical\n4. Other");
+                var gTypeValues = Enum.GetValues(typeof(GoodsType)).Cast<int>().Select(x => x.ToString()).ToArray();
+
+                var gType = Console.ReadLine();
+                if (!gTypeValues.Contains(gType))
+                {
+                    throw new Exception("Invalid type");
+                }
+                var goodsType = (GoodsType)Enum.Parse(typeof(GoodsType), gType);
+
+                Console.WriteLine("Do you want to add more items? (Y/N) [any other key except y/Y is considered as N]");
+                var input = Console.ReadLine().ToLower();
+
+                var product = productFactory.GetProduct(name, Convert.ToDecimal(price), Convert.ToInt32(quantity), goodsType, isImported);
+                Product foundItem = cartItems.Find(x => (x.Name == name && x.Price == product.Price));
+                if (foundItem != null)
+                {
+                    foundItem.Quantity = foundItem.Quantity + product.Quantity;
+                }
+                else
+                {
+                    cartItems.Add(product);
+                }
+
+                if (input == "y")
+                {
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Your Billing details are below");
+            Console.WriteLine("------- Date : " + DateTime.Now.ToString("dd/MM/yyyy") + " -------");
+            Console.WriteLine("");
+
+            foreach (var item in cartItems)
+            {
+                Console.Write(item.Name);
+                Console.Write("          "
+                            + item.getItemTotal().ToString("C"));
+                if (item.Quantity > 1)
+                {
+                    Console.Write(" (" + item.Quantity + " @ " + item.getPrice() + ") ");
+                }
+                Console.WriteLine("\n");
+            }
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("----------------- Total Items : {" + cartItems.Sum(item => item.Quantity) + "} -----------------\n");
+            Console.WriteLine("Tax : " + cartItems.Sum(item => (item.CalculateItemTax() * item.Quantity)).ToString("C"));
+            Console.WriteLine("Total : " + cartItems.Sum(item => item.getItemTotal()).ToString("C"));
+
+            Console.ReadLine();
+        }
+    }
+}
